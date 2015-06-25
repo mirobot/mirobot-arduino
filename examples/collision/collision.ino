24a16232b1f237575054130f5cf726396d79a763
Ben Pirt <ben@pirt.co.uk>
2015-06-25 18:29:39 +0100

    Fix the collision example

--- examples/collision/collision.ino
+++ examples/collision/collision.ino
@@ -6,7 +6,7 @@ This sketch demonstrates simple collision detection without using the built in l
 */
 
 Mirobot mirobot;
-typedef enum {NORMAL, RIGHT_REVERSE, RIGHT_TURN, LEFT_REVERSE, LEFT_TURN} collideState_ty;
+typedef enum {M_NORMAL, M_RIGHT_REVERSE, M_RIGHT_TURN, M_LEFT_REVERSE, M_LEFT_TURN} collideState_ty;
 collideState_ty collideState;
 
 void setup(){
@@ -16,29 +16,29 @@ void setup(){
 void loop(){
   boolean collideLeft = !digitalRead(LEFT_COLLIDE_SENSOR);
   boolean collideRight = !digitalRead(RIGHT_COLLIDE_SENSOR);
-  if(collideState == NORMAL){
+  if(collideState == M_NORMAL){
     if(collideLeft){
-      collideState = LEFT_REVERSE;
+      collideState = M_LEFT_REVERSE;
       mirobot.back(50);
     }else if(collideRight){
-      collideState = RIGHT_REVERSE;
+      collideState = M_RIGHT_REVERSE;
       mirobot.back(50);
     }else{
       mirobot.forward(10);
     }
-  }else if(motor1.ready() && motor2.ready()){
+  }else if(mirobot.ready()){
     switch(collideState){
-      case LEFT_REVERSE :
-        collideState = LEFT_TURN;
+      case M_LEFT_REVERSE :
+        collideState = M_LEFT_TURN;
         mirobot.right(90);
         break;
-      case RIGHT_REVERSE :
-        collideState = RIGHT_TURN;
+      case M_RIGHT_REVERSE :
+        collideState = M_RIGHT_TURN;
         mirobot.left(90);
         break;
-      case LEFT_TURN :
-      case RIGHT_TURN :
-        collideState = NORMAL;
+      case M_LEFT_TURN :
+      case M_RIGHT_TURN :
+        collideState = M_NORMAL;
     }
   }
 }
